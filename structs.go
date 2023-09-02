152df60673bb5c3e537758bdfd5382a8b461f31c
Yui <dev@sleepyyui.com>
2023-09-02 13:58:44 +0200

    fix bad indent

--- structs.go
+++ structs.go
@@ -1,85 +1,84 @@
 package amarigo
 
 type AmariBot struct {
-    ApiKey      string
-    BaseURL     string
-    CustomFetch string
-    Debug       bool
-    Version     string
+	ApiKey      string
+	BaseURL     string
+	CustomFetch string
+	Debug       bool
+	Version     string
 }
 
 type AmariError struct {
-    Message string
-    Name    string
-    Status  int
+	Message string
+	Name    string
+	Status  int
 }
 
 type RatelimitError struct {
-    Message   string
-    Name      string
-    Status    int
-    Remaining int
+	Message   string
+	Name      string
+	Status    int
+	Remaining int
 }
 
 type RequestHandler struct {
-    _client *AmariBot
+	_client *AmariBot
 }
 
 type Leaderboard struct {
-    Users []*LeaderboardUser `json:"data"`
-    Count int                `json:"count"`
-    Total int                `json:"total_count"`
+	Users []*LeaderboardUser `json:"data"`
+	Count int                `json:"count"`
+	Total int                `json:"total_count"`
 }
 
 type LeaderboardRaw struct {
-    Users []*LeaderboardUser `json:"data"`
-    Count int                `json:"count"`
+	Users []*LeaderboardUser `json:"data"`
+	Count int                `json:"count"`
 }
 
 type LeaderboardUser struct {
-    ID        string `json:"id"`
-    Username  string `json:"username"`
-    Exp       int    `json:"exp"`
-    Level     int    `json:"level"`
-    WeeklyExp int    `json:"weeklyExp"`
+	ID        string `json:"id"`
+	Username  string `json:"username"`
+	Exp       int    `json:"exp"`
+	Level     int    `json:"level"`
+	WeeklyExp int    `json:"weeklyExp"`
 }
 
 type Weekly struct {
-    Users []*WeeklyUser `json:"data"`
-    Count int           `json:"count"`
-    Total int           `json:"total_count"`
+	Users []*WeeklyUser `json:"data"`
+	Count int           `json:"count"`
+	Total int           `json:"total_count"`
 }
 
 type WeeklyRaw struct {
-    Users []*WeeklyUser `json:"data"`
-    Count int           `json:"count"`
+	Users []*WeeklyUser `json:"data"`
+	Count int           `json:"count"`
 }
 
 type WeeklyUser struct {
-    ID       string `json:"id"`
-    Username string `json:"username"`
-    Exp      int    `json:"exp"`
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Exp      int    `json:"exp"`
 }
 
 type User struct {
-    ID string `json:"id"`
-    
-    Username  string `json:"username"`
-    Exp       int    `json:"exp"`
-    Level     int    `json:"level"`
-    WeeklyExp int    `json:"weeklyExp"`
+	ID        string `json:"id"`
+	Username  string `json:"username"`
+	Exp       int    `json:"exp"`
+	Level     int    `json:"level"`
+	WeeklyExp int    `json:"weeklyExp"`
 }
 
 type Rewards struct {
-    Rewards []*Reward `json:"data"`
-    Count   int       `json:"count"`
+	Rewards []*Reward `json:"data"`
+	Count   int       `json:"count"`
 }
 
 type Reward struct {
-    RoleID string `json:"role_id"`
-    Level  int    `json:"level"`
+	RoleID string `json:"role_id"`
+	Level  int    `json:"level"`
 }
 
 type GetGuildMembers struct {
-    Members []*string
+	Members []*string
 }
