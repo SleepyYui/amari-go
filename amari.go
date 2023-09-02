c31f0b88a026afb45b73b97fb163fa53cacd9a74
Yui <dev@sleepyyui.com>
2023-09-02 13:57:19 +0200

    Reindent to spaces instead of tab

--- amari.go
+++ amari.go
@@ -1,11 +1,11 @@
 package amarigo
 
 import (
-	"bytes"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
+    "bytes"
+    "encoding/json"
+    "fmt"
+    "io"
+    "net/http"
 )
 
 var client = &http.Client{}
@@ -14,207 +14,207 @@ const version = "0.1.0"
 
 func New(token string) *AmariBot {
 
-	bot := &AmariBot{
-		ApiKey:  token,
-		BaseURL: "https://amaribot.com/api/v1",
-		Debug:   false,
-		Version: version,
-	}
+    bot := &AmariBot{
+        ApiKey:  token,
+        BaseURL: "https://amaribot.com/api/v1",
+        Debug:   false,
+        Version: version,
+    }
 
-	_, resp, err := bot.fetch("/guilds/1/leaderboard")
-	if resp.StatusCode == 403 {
-		panic("Invalid API key")
-	} else if err != nil {
-		panic(err)
-	}
-
-	return bot
+    _, resp, err := bot.fetch("/guilds/1/leaderboard")
+    if resp.StatusCode == 403 {
+        panic("Invalid API key")
+    } else if err != nil {
+        panic(err)
+    }
+    
+    return bot
 }
 
 func (bot *AmariBot) printDebug(data interface{}) {
-	if bot.Debug {
-		fmt.Print("DEBUG: ")
-		fmt.Println(data)
-	}
+    if bot.Debug {
+        fmt.Print("DEBUG: ")
+        fmt.Println(data)
+    }
 }
 
 func (bot *AmariBot) fetch(url string) (body []byte, resp *http.Response, err error) {
-	url = fmt.Sprintf("%s%s", bot.BaseURL, url)
+    url = fmt.Sprintf("%s%s", bot.BaseURL, url)
 
-	bot.printDebug(url)
+    bot.printDebug(url)
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return
-	}
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        return
+    }
 
-	req.Header.Set("Authorization", bot.ApiKey)
-	req.Header.Set("User-Agent", fmt.Sprintf("amarigo/%s", bot.Version))
+    req.Header.Set("Authorization", bot.ApiKey)
+    req.Header.Set("User-Agent", fmt.Sprintf("amarigo/%s", bot.Version))
 
-	resp, err = client.Do(req)
-	if err != nil {
-		return
-	}
+    resp, err = client.Do(req)
+    if err != nil {
+        return
+    }
 
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			return
-		}
-	}(resp.Body)
+    defer func(Body io.ReadCloser) {
+        err := Body.Close()
+        if err != nil {
+            return
+        }
+    }(resp.Body)
 
-	body, err = io.ReadAll(resp.Body)
+    body, err = io.ReadAll(resp.Body)
 
-	bot.printDebug(string(body))
+    bot.printDebug(string(body))
 
-	return
+    return
 }
 
 func (bot *AmariBot) fetchBody(url string, inputBody []byte) (body []byte, resp *http.Response, err error) {
-	url = fmt.Sprintf("%s%s", bot.BaseURL, url)
+    url = fmt.Sprintf("%s%s", bot.BaseURL, url)
 
-	bot.printDebug(url)
+    bot.printDebug(url)
 
-	req, err := http.NewRequest("GET", url, nil)
-	if err != nil {
-		return
-	}
+    req, err := http.NewRequest("GET", url, nil)
+    if err != nil {
+        return
+    }
 
-	req.Header.Set("Authorization", bot.ApiKey)
-	req.Header.Set("User-Agent", fmt.Sprintf("amarigo/%s", bot.Version))
+    req.Header.Set("Authorization", bot.ApiKey)
+    req.Header.Set("User-Agent", fmt.Sprintf("amarigo/%s", bot.Version))
 
-	req.Body = io.NopCloser(bytes.NewReader(inputBody))
+    req.Body = io.NopCloser(bytes.NewReader(inputBody))
 
-	resp, err = client.Do(req)
-	if err != nil {
-		return
-	}
+    resp, err = client.Do(req)
+    if err != nil {
+        return
+    }
 
-	defer func(Body io.ReadCloser) {
-		err := Body.Close()
-		if err != nil {
-			return
-		}
-	}(resp.Body)
+    defer func(Body io.ReadCloser) {
+        err := Body.Close()
+        if err != nil {
+            return
+        }
+    }(resp.Body)
 
-	body, err = io.ReadAll(resp.Body)
+    body, err = io.ReadAll(resp.Body)
 
-	bot.printDebug(string(body))
+    bot.printDebug(string(body))
 
-	return
+    return
 }
 
 func (bot *AmariBot) GetLeaderboardPage(guildID string, page int) (leaderboard *Leaderboard, err error) {
 
-	url := fmt.Sprintf("/guild/leaderboard/%s?page=%d", guildID, page)
+    url := fmt.Sprintf("/guild/leaderboard/%s?page=%d", guildID, page)
 
-	body, _, err := bot.fetch(url)
-	if err != nil {
-		return nil, err
-	}
+    body, _, err := bot.fetch(url)
+    if err != nil {
+        return nil, err
+    }
 
-	bot.printDebug(body)
-	err = json.Unmarshal(body, &leaderboard)
-	bot.printDebug(leaderboard)
+    bot.printDebug(body)
+    err = json.Unmarshal(body, &leaderboard)
+    bot.printDebug(leaderboard)
 
-	return
+    return
 }
 
 func (bot *AmariBot) GetLeaderboardRaw(guildID string) (leaderboard *LeaderboardRaw, err error) {
 
-	url := fmt.Sprintf("/guild/raw/leaderboard/%s", guildID)
+    url := fmt.Sprintf("/guild/raw/leaderboard/%s", guildID)
 
-	body, _, err := bot.fetch(url)
-	if err != nil {
-		return nil, err
-	}
+    body, _, err := bot.fetch(url)
+    if err != nil {
+        return nil, err
+    }
 
-	bot.printDebug(body)
-	err = json.Unmarshal(body, &leaderboard)
-	bot.printDebug(leaderboard)
+    bot.printDebug(body)
+    err = json.Unmarshal(body, &leaderboard)
+    bot.printDebug(leaderboard)
 
-	return
+    return
 }
 
 func (bot *AmariBot) GetWeekly(guildID string) (weekly *Weekly, err error) {
 
-	url := fmt.Sprintf("/guild/weekly/%s", guildID)
+    url := fmt.Sprintf("/guild/weekly/%s", guildID)
 
-	body, _, err := bot.fetch(url)
-	if err != nil {
-		return nil, err
-	}
+    body, _, err := bot.fetch(url)
+    if err != nil {
+        return nil, err
+    }
 
-	bot.printDebug(body)
-	err = json.Unmarshal(body, &weekly)
-	bot.printDebug(weekly)
+    bot.printDebug(body)
+    err = json.Unmarshal(body, &weekly)
+    bot.printDebug(weekly)
 
-	return
+    return
 }
 
 func (bot *AmariBot) GetWeeklyRaw(guildID string) (weekly *WeeklyRaw, err error) {
 
-	url := fmt.Sprintf("/guild/raw/weekly/%s", guildID)
+    url := fmt.Sprintf("/guild/raw/weekly/%s", guildID)
 
-	body, _, err := bot.fetch(url)
-	if err != nil {
-		return nil, err
-	}
+    body, _, err := bot.fetch(url)
+    if err != nil {
+        return nil, err
+    }
 
-	bot.printDebug(body)
-	err = json.Unmarshal(body, &weekly)
-	bot.printDebug(weekly)
+    bot.printDebug(body)
+    err = json.Unmarshal(body, &weekly)
+    bot.printDebug(weekly)
 
-	return
+    return
 }
 
 func (bot *AmariBot) GetGuildMember(guildID, userID string) (member *User, err error) {
 
-	url := fmt.Sprintf("/guild/%s/member/%s", guildID, userID)
+    url := fmt.Sprintf("/guild/%s/member/%s", guildID, userID)
 
-	body, _, err := bot.fetch(url)
-	if err != nil {
-		return nil, err
-	}
+    body, _, err := bot.fetch(url)
+    if err != nil {
+        return nil, err
+    }
 
-	bot.printDebug(body)
-	err = json.Unmarshal(body, &member)
-	bot.printDebug(member)
+    bot.printDebug(body)
+    err = json.Unmarshal(body, &member)
+    bot.printDebug(member)
 
-	return
+    return
 }
 
 func (bot *AmariBot) GetGuildMembers(guildID string, members GetGuildMembers) (users []*User, err error) {
 
-	url := fmt.Sprintf("/guild/%s/members", guildID)
+    url := fmt.Sprintf("/guild/%s/members", guildID)
 
-	memberBody, err := json.Marshal(members.Members)
+    memberBody, err := json.Marshal(members.Members)
 
-	body, _, err := bot.fetchBody(url, memberBody)
-	if err != nil {
-		return nil, err
-	}
+    body, _, err := bot.fetchBody(url, memberBody)
+    if err != nil {
+        return nil, err
+    }
 
-	bot.printDebug(body)
-	err = json.Unmarshal(body, &members)
-	bot.printDebug(members)
+    bot.printDebug(body)
+    err = json.Unmarshal(body, &members)
+    bot.printDebug(members)
 
-	return
+    return
 
 }
 
 func (bot *AmariBot) GetGuildRewards(guildID string) (rewards []*Reward, err error) {
 
-	url := fmt.Sprintf("/guild/rewards/%s", guildID)
+    url := fmt.Sprintf("/guild/rewards/%s", guildID)
 
-	body, _, err := bot.fetch(url)
-	if err != nil {
-		return nil, err
-	}
+    body, _, err := bot.fetch(url)
+    if err != nil {
+        return nil, err
+    }
 
-	bot.printDebug(body)
-	err = json.Unmarshal(body, &rewards)
-	bot.printDebug(rewards)
+    bot.printDebug(body)
+    err = json.Unmarshal(body, &rewards)
+    bot.printDebug(rewards)
 
-	return
+    return
 }
