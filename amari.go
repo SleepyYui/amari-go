5dbfcb55b2edcc4c374a2c90e8133dbb1ba09a01
Yui <dev@sleepyyui.com>
2023-09-02 12:42:59 +0200

    add few functions

--- amari.go
+++ amari.go
@@ -1,27 +1,72 @@
 package amarigo
 
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+var client = &http.Client{}
+
 const version = "0.1.0"
 
 func New(token string) *AmariBot {
 
 	bot := &AmariBot{
-		apiKey:  token,
-		baseURL: "https://amaribot.com/api/v1",
-		debug:   false,
-		version: version,
+		ApiKey:  token,
+		BaseURL: "https://amaribot.com/api/v1",
+		Debug:   false,
+		Version: version,
 	}
 
 	return bot
 }
 
-func (bot *AmariBot) getBulkUserLevel(guildID string, userID string) (string, error) {
-	return "", nil
+func (bot *AmariBot) printDebug(data interface{}) {
+	if bot.Debug {
+		fmt.Println(data)
+	}
 }
 
-func (bot *AmariBot) getCombinedLeaderboard(guildID string, userID string) (string, error) {
-	return "", nil
+func (bot *AmariBot) fetch(url string, target interface{}) (body []byte, err error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return
+	}
+
+	req.Header.Set("Authorization", bot.ApiKey)
+	req.Header.Set("User-Agent", fmt.Sprintf("amarigo/%s", bot.Version))
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			return
+		}
+	}(resp.Body)
+
+	body, err = io.ReadAll(resp.Body)
+	return
 }
 
-func (bot *AmariBot) getGuildRewards(guildID string, options PaginationOptions) (string, error) {
-	return "", nil
+func (bot *AmariBot) getLeaderboardPage(guildID string, page int) (leaderboard *Leaderboard, err error) {
+
+	url := fmt.Sprintf("%s/guilds/%s/leaderboard?page=%d", bot.BaseURL, guildID, page)
+
+	body, err := bot.fetch(url, &leaderboard)
+	if err != nil {
+		return nil, err
+	}
+
+	bot.printDebug(body)
+
+	err = json.Unmarshal(body, &leaderboard)
+	bot.printDebug(leaderboard)
+
+	return
 }
--- structs.go
+++ structs.go
@@ -1,107 +1,40 @@
 package amarigo
 
 type AmariBot struct {
-	apiKey      string
-	baseURL     string
-	customFetch string
-	debug       bool
-	version     string
+	ApiKey      string
+	BaseURL     string
+	CustomFetch string
+	Debug       bool
+	Version     string
 }
 
 type AmariError struct {
-	message string
-	name    string
-	status  int
+	Message string
+	Name    string
+	Status  int
 }
 
 type RatelimitError struct {
-	message   string
-	name      string
-	status    int
-	remaining int
+	Message   string
+	Name      string
+	Status    int
+	Remaining int
 }
 
 type RequestHandler struct {
 	_client *AmariBot
 }
 
-type APIBulkUsers struct {
-	members         []APIUser
-	queried_members int
-	total_members   int
+type Leaderboard struct {
+	Data  []LeaderboardUser `json:"current_page"`
+	Count int               `json:"count"`
+	Total int               `json:"total_count"`
 }
 
-type APIError struct {
-	error string
-}
-
-type APILeaderboard struct {
-	count       int
-	data        []APILeaderboardUser
-	total_count int
-}
-
-type APILeaderboardUser struct {
-	exp      int
-	id       string
-	level    int
-	username string
-}
-
-type APIRawLeaderboard struct {
-	count int
-	data  []APILeaderboardUser
-}
-
-type APIRawWeeklyLeaderboard struct {
-	count int
-	data  []APIWeeklyUser
-}
-
-type APIRewards struct {
-	count int
-	data  []APIReward
-}
-
-type APIReward struct {
-	level  int
-	roleID string
-}
-
-type APIUser struct {
-	exp       int
-	id        string
-	level     int
-	username  string
-	weeklyExp int
-}
-
-type APIWeeklyLeaderboard struct {
-	count       int
-	data        []APIWeeklyUser
-	total_count int
-}
-
-type APIWeeklyUser struct {
-	exp      int
-	id       string
-	username string
-}
-
-type CustomFetch struct {
-	url     string
-	options map[string]string
-}
-
-type CustomResponse struct {
-	body string
-}
-
-type PaginationOptions struct {
-	limit int
-	page  int
-}
-
-type RawPaginationOptions struct {
-	limit int
+type LeaderboardUser struct {
+	ID        string `json:"id"`
+	Username  string `json:"username"`
+	Exp       int    `json:"exp"`
+	Level     int    `json:"level"`
+	WeeklyExp int    `json:"weeklyExp"`
 }
